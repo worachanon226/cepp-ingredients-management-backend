d2d571326b5d82353c3d1cffd22191734708cf9d
PuNe <worachanon226@hotmail.com>
2024-01-19 15:35:40 +0700

    update: config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  if (configService.get<string>('VERSION') == 'TEST') {
+    const config = new DocumentBuilder()
+      .setTitle('Ingredients-management')
+      .setDescription('Ingredients-management API')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
