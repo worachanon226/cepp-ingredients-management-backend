d2ddc3ba964cb0cd8a2db721976333304deb9bad
PuNe <worachanon226@hotmail.com>
2024-01-20 16:19:08 +0700

    update: role guard

--- src/modules/auth/guard/auth.guard.ts
+++ src/modules/auth/guard/auth.guard.ts
@@ -25,7 +25,6 @@ export class AuthGuard implements CanActivate {
       });
       request['user'] = payload;
     } catch (err) {
-      console.log(err);
       throw new UnauthorizedException();
     }
     return true;
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { AuthGuard } from '../auth/guard/auth.guard';
+import { RolesGuard } from '../auth/guard/role.guard';
+import { Roles } from '../auth/decorator/role.decorator';
+import { UserRole } from './schema/user.schema';
 
 @ApiTags('user')
 @Controller('user')
@@ -10,7 +13,8 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
-  @UseGuards(AuthGuard)
+  @Roles(UserRole.OWNER)
+  @UseGuards(AuthGuard, RolesGuard)
   async getAll() {
     return await this.userService.getAll();
   }
