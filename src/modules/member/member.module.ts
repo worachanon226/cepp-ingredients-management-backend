a4cc1193420aa6a39bfc798fdd86bee5a89de8b9
PuNe <worachanon226@hotmail.com>
2024-01-20 21:00:01 +0700

    create: member module

---
+++ src/modules/member/member.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MemberService } from './member.service';
+import { MemberController } from './member.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Member, MemberSchema } from './schema/member.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Member.name, schema: MemberSchema }]),
+  ],
+  controllers: [MemberController],
+  providers: [MemberService],
+  exports: [MemberService],
+})
+export class MemberModule {}
