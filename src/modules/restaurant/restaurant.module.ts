380aedaec333b16079fedc9f60033d39d60aa3a0
PuNe <worachanon226@hotmail.com>
2024-01-21 20:21:10 +0700

    Revert "create: member module"
    This reverts commit a4cc1193420aa6a39bfc798fdd86bee5a89de8b9.

--- src/modules/restaurant/restaurant.module.ts
+++ src/modules/restaurant/restaurant.module.ts
@@ -3,14 +3,12 @@ import { RestaurantController } from './restaurant.controller';
 import { RestaurantService } from './restaurant.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Restaurant, RestaurantSchema } from './schema/restaurant.schema';
-import { MemberModule } from '../member/member.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Restaurant.name, schema: RestaurantSchema },
     ]),
-    MemberModule,
   ],
   controllers: [RestaurantController],
   providers: [RestaurantService],
