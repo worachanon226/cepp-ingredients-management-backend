287c2f575ac1024b29441a5ac6214e10031c67c2
PuNe <worachanon226@hotmail.com>
2024-01-21 20:26:24 +0700

    Revert "Revert "create: member module""
    This reverts commit 380aedaec333b16079fedc9f60033d39d60aa3a0.

---
+++ src/modules/member/member.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import mongoose from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Member } from './schema/member.schema';
+
+@Injectable()
+export class MemberService {
+  constructor(
+    @InjectModel(Member.name)
+    private readonly memberModel: mongoose.Model<Member>,
+  ) {}
+
+  async create(userId: string, restaurantId: string) {
+    const createdMember = new this.memberModel({
+      userId: userId,
+      restaurantId: restaurantId,
+    });
+    await createdMember.save();
+  }
+
+  async findByRestaurantId(restaurantId: string) {
+    return await this.memberModel.find({ restaurantId: restaurantId });
+  }
+
+  async findByUserId(userId: string) {
+    return await this.memberModel.find({ userId: userId });
+  }
+
+  async deleteUser(userId: string, restaurantId: string) {
+    return await this.memberModel.findOneAndDelete({
+      userId: userId,
+      restaurantId: restaurantId,
+    });
+  }
+}
